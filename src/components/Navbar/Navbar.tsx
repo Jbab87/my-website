448e08eb7aa91218df97bb3463b51cfa1a272ab0
Jbab87 <jbabajide87@gmail.com>
2024-06-13 18:10:43 +0100

    This commit adds more styling and nav links to navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,23 +1,23 @@
 import { useState } from 'react'
 import './Navbar.css'
+import { Link } from 'react-router-dom'
 
 function Navbar() {
   const [] = useState(0)
 
   return (
-      <div className='navbar'>
-        <div className='navbar-contents'>
-        <div className='title-container'> 
-        <h2 className='title'>Rent4Cheap</h2>
+    <nav className='navbar'>
+        <div className='navbar-container'>
+        <div className='navbar-logo-container'> 
+        <h2><Link className='navbar-logo' to={'/#'}>Rent4Cheap</Link></h2>
         </div>
         <ul>
-          <li>Products</li>
-          <li>Blog</li>
-          <li>About</li>
+          <li><Link className='list-item' to={'/#'}>Products</Link></li>
+          <li><Link className='list-item' to={'/about'}>Blog</Link></li>
+          <li><Link className='list-item' to={'/about'}>About</Link></li>
         </ul>
         </div>
-      </div>
-    
+      </nav> 
   )
 }
 
