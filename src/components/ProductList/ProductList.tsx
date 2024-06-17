8054ef2ba437f4ef316f6f682d5ec556f67dfc82
Jbab87 <jbabajide87@gmail.com>
2024-06-17 18:03:03 +0100

    This commit adds more styling to the product cards

--- src/components/ProductList/ProductList.tsx
+++ src/components/ProductList/ProductList.tsx
@@ -3,17 +3,22 @@ import './ProductList.css'
 import productList from '../../../products.json'
 
 function ProductList() {
-  const [products, setProducts] = useState(productList)
+  const [] = useState(productList)
 
 let x =  productList.map((product) => {
     return (
-      <div className='product-card'>
+      <div className='product-card'> 
+      <div className='image-container'>
          <div className='product-image'>
             
         </div>
-        <a href='https://fatllama.com/uk/i/hire-go-pro-hero9-camera-58788046'><h3>{product.name}</h3> </a>
-        <p>£{product.price}</p>
+      
       </div>
+        <div className='product-info'> 
+         <a href='https://fatllama.com/uk/i/hire-go-pro-hero9-camera-58788046'><h3>{product.name}</h3> </a>
+        <p>£{product.price}</p>
+        </div>
+        </div>
     )
   })
 
