c6e0159e7f017e04f4a04a90054fd7573359af70
Jbab87 <jbabajide87@gmail.com>
2024-06-29 14:23:42 +0100

    This commit adds styling of home page for 480px breakpoint

--- src/components/ProductLink/ProductLink.tsx
+++ src/components/ProductLink/ProductLink.tsx
@@ -3,27 +3,12 @@ import './ProductLink.css'
 import productList from '../../../products.json'
 import drone from "../../assets/zac-gudakov-0addIoploTs-unsplash.jpg"
 import gopro from "../../assets/jye-b-DGyTUaS6_aw-unsplash.jpg"
+import rode from "../../assets/dimitri-karastelev-AuVIIdp7hfk-unsplash.jpg"
 import Card from '../Card/Card'
 
 function ProductLink() {
   const [] = useState(productList)
 
-let x =  productList.map((product) => {
-    return (
-      <div className='product-card'> 
-      <div className='image-container'>
-         <div className='product-image'>
-            
-        </div>
-      
-      </div>
-        <div className='product-info'> 
-         <a href='https://fatllama.com/uk/i/hire-go-pro-hero9-camera-58788046'><h3>{product.name}</h3> </a>
-        <p>£{product.price}</p>
-        </div>
-        </div>
-    )
-  })
 
   const goProStyle = {
       backgroundImage: `url(${gopro})`
@@ -33,12 +18,16 @@ let x =  productList.map((product) => {
     backgroundImage: `url(${drone})`
 }
 
+const rodeStyle = {
+  backgroundImage : `url(${rode})`
+}
+
   return (
     <div className='outlet-ctr'> 
 
         <div className='content-ctr'>
-            <div className='content-ctr-txt'> 
-            <h1>Save on Products below</h1>
+            <div className='content-ctr-txt-1'> 
+            <h2>Save on Products below</h2>
             </div>
 
             <div className='cards-ctr'> 
@@ -53,10 +42,16 @@ let x =  productList.map((product) => {
                 title={"Go Pro Action Cameras"}
                 text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ex nulla, consectetur vel suscipit ut, cursus vitae eros. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque dictum urna sodales quam iaculis, et luctus elit fringilla. Vestibulum condimentum venenatis felis, ut placerat neque hendrerit ac. Nullam scelerisque viverra enim id ornare. Mauris mattis accumsan porttitor. Pellentesque cursus tortor eget molestie scelerisque. Mauris aliquam ligula nec massa iaculis porttitor."}
                 />
+                <Card
+                backgroundImg={rodeStyle}
+                title={"Go Pro Action Cameras"}
+                text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ex nulla, consectetur vel suscipit ut, cursus vitae eros. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque dictum urna sodales quam iaculis, et luctus elit fringilla. Vestibulum condimentum venenatis felis, ut placerat neque hendrerit ac. Nullam scelerisque viverra enim id ornare. Mauris mattis accumsan porttitor. Pellentesque cursus tortor eget molestie scelerisque. Mauris aliquam ligula nec massa iaculis porttitor."}
+                />
+
            </div>
 
-          <div className='content-ctr-txt'> 
-            <h1>More to come!</h1>
+          <div className='content-ctr-txt-2'> 
+            <h2>And many more items!</h2>
           </div>
            
       </div>
