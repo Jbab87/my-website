38acd0c198f755a001a7aac9a369cc9e10961e38
Jbab87 <jbabajide87@gmail.com>
2024-08-27 21:35:25 +0100

    This commit fixes some  bugs seen in deployed site

--- src/components/Feature/Feature.tsx
+++ src/components/Feature/Feature.tsx
@@ -3,7 +3,7 @@ import './Feature.css'
 function Feature(props: any) {
   return (
       <div className='desc-ftr'> 
-      <h2>{props.title}</h2>
+      <h2 className='desc-title'>{props.title}</h2>
       <div className='desc-icon-ctr'>
         <span className="material-symbols-outlined">{props.icon}</span>
         </div>
